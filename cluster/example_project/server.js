fe1f410cf6e60ff77bb8829e07d86f27d28d93a7
jonatasfreitasv <jonatasfreitasv@gmail.com>
2017-10-26 23:50:29 -0200

    Same refactors

--- cluster/example_project/server.js
+++ cluster/example_project/server.js
@@ -2,13 +2,13 @@ var http = require('http');
 
 var handleRequest = function(request, response) {
 
-    console.log('Received request for URL: ' + request.url);
+    console.log(`${Date.now()} -> Hello World!` + request.url);
     response.writeHead(200);
-    response.end('Hello World!');
+    response.end(`${Date.now()} -> Hello World!`);
 
 };
 
 
 var www = http.createServer(handleRequest);
-www.listen(80);
-console.log('Server listen on 80');
+www.listen(5000);
+console.log('Server listen on 5000');
