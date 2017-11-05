55cfef179ea0dca4715838b669e840d704087536
jonatasfreitasv <jonatasfreitasv@gmail.com>
2017-11-05 19:44:11 -0200

    checkpoint

--- socket_io/src/server.js
+++ socket_io/src/server.js
@@ -10,6 +10,12 @@ io.adapter(redis({ host: '192.168.0.101', port: 6379, password: 'abc123' }));
 
 sdc.gauge('socketio.connected_clients', 0);
 
+io.on('error', err => {
+
+    console.log(err);
+
+});
+
 io.on('connection', socket => {
 
     console.log(`Client connected - Number of clients ${Object.keys(io.sockets.sockets).length}`);
