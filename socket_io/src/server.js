be352066b38f2f354af9700f72801ee238a7ac30
jonatasfreitasv <jonatasfreitasv@gmail.com>
2017-11-05 19:44:18 -0200

    checkpoint

--- socket_io/src/server.js
+++ socket_io/src/server.js
@@ -11,9 +11,7 @@ io.adapter(redis({ host: '192.168.0.101', port: 6379, password: 'abc123' }));
 sdc.gauge('socketio.connected_clients', 0);
 
 io.on('error', err => {
-
     console.log(err);
-
 });
 
 io.on('connection', socket => {
