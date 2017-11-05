4080ead9f1ec3afea294e653e05536e31abef264
jonatasfreitasv <jonatasfreitasv@gmail.com>
2017-11-05 19:43:18 -0200

    checkpoint

--- socket_io/src/server.js
+++ socket_io/src/server.js
@@ -2,16 +2,11 @@ const figlet = require('figlet');
 const redis = require('socket.io-redis');
 const io = require('socket.io')(process.env.SOCKET_PORT_SERVER || 8080);
 const SDC = require('statsd-client');
-const restify = require('restify');
-const _package = require('../package.json');
 
 require('dotenv').config();
 
 let sdc = new SDC({host: process.env.STATSD_HOST || '127.0.0.1', port: process.env.STATSD_PORT || 8125, debug: false});
 io.adapter(redis({ host: '192.168.0.101', port: 6379, password: 'abc123' }));
-
-
-const server = restify.createServer({ name: _package.name, version: _package.version });
 
 sdc.gauge('socketio.connected_clients', 0);
 
@@ -46,8 +41,4 @@ figlet('SocketPI Cluster - Server', (err, data) => {
     console.log(data);
     console.log(`- start on port ${process.env.SOCKET_PORT_SERVER || 8080}`)
     console.log(`statsd host ${process.env.STATSD_HOST}`)
-});
-
-server.listen(80, function () {
-    console.log('%s listening at %s', server.name, server.url);
 });
