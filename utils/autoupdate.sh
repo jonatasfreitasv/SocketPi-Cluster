3a882c90cbe25fa1efe29da98602db6b288ca541
jonatasfreitasv <jonatasfreitasv@gmail.com>
2017-11-06 21:32:30 -0200

    checkpoint

--- utils/autoupdate.sh
+++ utils/autoupdate.sh
@@ -6,4 +6,4 @@ tmux -2 new-session -d -s $SESSION
 tmux new-window -t $SESSION:1 -n 'Autoupdate'
 
 tmux select-pane -t 0
-tmux send-keys "git add . && git commit -m 'checkpoint' && git push origin master" C-m
+tmux send-keys "watch git add . && git commit -m 'checkpoint' && git push origin master" C-m
