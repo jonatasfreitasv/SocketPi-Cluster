84a46033c0910c63e3459218a0d81d331784e6ac
jonatasfreitasv <jonatasfreitasv@gmail.com>
2017-11-06 22:13:13 -0200

    checkpoint

--- socket_io/src/client.js
+++ socket_io/src/client.js
@@ -20,8 +20,9 @@ io.on('connect', function(){
 
         const current_microsecond = process.hrtime()[0] * 1000000 + process.hrtime()[1] / 1000;
 
-        io.emit('event', new Date());
-        console.log(`Send data in ${new Date()}`)
+        io.emit('event', ncurrent_microsecond);
+        console.log(`Send data in ${current_microsecond}`)
+
     }, process.env.CLIENT_SEND_DATA_INTERVAL_SECONDS)
 
 });
